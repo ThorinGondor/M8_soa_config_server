29dfe0a5305a8c7d0e2acb60e71d114f483372ab
Asuka <1360002051@qq.com>
2019-02-01 14:26:29 +0800

    服务端

---
+++ src/main/java/com/config_demo/soa_config_server/SoaConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.config_demo.soa_config_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer //开启Spring Cloud Config 服务端功能
+@SpringBootApplication
+public class SoaConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoaConfigServerApplication.class, args);
+    }
+
+}
+
+//启动后请访问：http://localhost:7001/spring-cloud-config-client-dev.properties
+
