d050828f522139ee04c96b19fd662835ceed1de4
LiXinze <1360002051@qq.com>
2019-02-03 09:17:55 +0800

    SpringCloud Bus RabbitMQ

--- src/main/java/com/config_demo/soa_config_server/SoaConfigServerApplication.java
+++ src/main/java/com/config_demo/soa_config_server/SoaConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.config_demo.soa_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableDiscoveryClient
 @EnableConfigServer //开启Spring Cloud Config 服务端功能
 @SpringBootApplication
 public class SoaConfigServerApplication {
